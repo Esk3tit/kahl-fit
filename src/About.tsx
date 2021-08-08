ab49804bbf4c9ed8db0e53f2c7dbac6638d429a7
Kai Pan <khaiphn41@gmail.com>
2021-08-07 18:37:43 -0700

    Ported over most of the content of the old website except for header and navbar and footer

---
+++ src/About.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Eddie Kahl is an ACSM certified fitness trainer in Portland, Oregon. 
+                Eddie has been resistance training for 3 years and strives to make a positive, 
+                healthy impact on all those wanting to use resistance training as a hobby and health remedy.
+            </p>
+            <p>
+                To see programs, offers, and rates, please visit the Services page.
+            </p>
+            <p>
+                For an initial consultation, please contact Eddie directly via text at <em>971-284-7214</em>. 
+            </p>
+        </div>
+    );
+}
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Policies from './Policies';
+import Services from './Services';
+import Location from './Location';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <About />
+      <Policies />
+      <Services />
+      <Location />
     </div>
   );
 }
