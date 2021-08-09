286b7ce74d30bc25fc62c65ad93c4f9b4d75788b
machj-sudo <jonathan.mach00@gmail.com>
2021-08-08 18:04:25 -0700

    remove duplicated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
+// import logo from './logo.svg
 import './App.css';
-import About from './About';
-import Policies from './Policies';
-import Services from './Services';
-import Location from './Location';
+import About from './components/About';
+import Policies from './components/Policies';
+import Services from './components/Services';
+import Location from './components/Location';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
       <About />
       <Policies />
