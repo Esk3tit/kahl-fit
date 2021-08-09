c4275c0072bdbcb31451aef33eeaa763617e1bae
machj-sudo <jonathan.mach00@gmail.com>
2021-08-08 21:50:36 -0700

    Added Navbar Header Component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import '../style/Navbar.css';
+
+const NavbarHeader = () => {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"> Picture Here </Navbar.Brand>
+                    <Nav>
+                        <Nav.Link href="/Home"> 
+                        Home 
+                        </Nav.Link>
+                        <Nav.Link href="/About"> 
+                        About 
+                        </Nav.Link>
+                        <Nav.Link href="/Policies"> 
+                        Policies 
+                        </Nav.Link>
+                        <Nav.Link href="/Services"> 
+                        Services 
+                        </Nav.Link>
+                        <Nav.Link href="/Location"> 
+                        Location 
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavbarHeader;
